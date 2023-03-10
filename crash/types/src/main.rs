b009e01e1ba0c49f5160f9656ae3305e50100d72
augustine <augustino0890@gmail.com>
2023-03-11 00:12:16 +0900

    rust-crash: organized types package

--- crash/types/src/main.rs
+++ crash/types/src/main.rs
@@ -1,5 +1,8 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_variables)]
+
+use types::{ding, on_off, print_array, print_difference, print_distance};
+
 fn main() {
     let coords: (f32, f32) = (6.3, 15.0);
 
@@ -14,31 +17,6 @@ fn main() {
     let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
     on_off(mess.2[0].0); // ignore
     on_off(mess.2[1].0);
-}
 
-fn print_difference(x: f32, y: f32) {
-    println!("Difference between {} and {} is {}", x, y, (x - y).abs());
-}
-
-fn print_array(a: [f32; 2]) {
-    println!("The coordinates are ({}, {})", a[0], a[1]);
-}
-
-fn ding(x: i32) {
-    if x == 13 {
-        println!("Ding, you found 13!");
-    }
-}
-
-fn on_off(val: bool) {
-    if val {
-        println!("Lights are on!");
-    }
-}
-
-fn print_distance(z: (f32, f32)) {
-    println!(
-        "Distance to the origin is {}",
-        (z.0.powf(2.0) + z.1.powf(2.0)).sqrt()
-    );
+    print_distance(coords);
 }
