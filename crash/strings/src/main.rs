802d72c823e7742bf8578d4d16eb1f250aee9de3
augustine <augustino0890@gmail.com>
2023-03-13 16:33:11 +0900

    crash-course: more control flow exercise

--- crash/strings/src/main.rs
+++ crash/strings/src/main.rs
@@ -13,6 +13,37 @@ fn main() {
             count(arg);
         }
     }
+
+    let mut count = 0u32;
+    let mut bunnies = 2u32;
+    while bunnies < 500 {
+        count += 1;
+        bunnies *= 2;
+    }
+    println!(
+        "Bunnies doubled {} times before there were more than 500",
+        count
+    );
+
+    let mut fives: Vec<i32> = vec![];
+    let mut numeber: i32 = 5;
+    for i in 1..=12 {
+        fives.push(i * numeber);
+    }
+    println!("Here are the first 12 multiples of 5: {:?}", fives);
+
+    let mut total = 0u32;
+    let numbers = vec![0, 1, 2, 3, 4, 5];
+    for number in numbers {
+        if number == 0 {
+            total += 7;
+        } else if number == 1 || number == 2 {
+            total += 30;
+        } else {
+            total -= 5;
+        }
+    }
+    println!("The total is {}", total);
 }
 
 fn sum() {
