e14b33b0ef7b66dfa05c62638b01e636595fdda8
augustine <augustino0890@gmail.com>
2023-03-21 17:34:03 +0900

    sum: handle errors with expect()

--- crash/sum/src/main.rs
+++ crash/sum/src/main.rs
@@ -16,12 +16,27 @@ fn main() {
         Ok(_) => println!("The second number is {}", second),
         Err(error) => println!("Error reading from stdin: {}", error),
     }
-    let b: u32 = second.trim().parse().unwrap();
+    // let b: u32 = second.trim().parse().unwrap();
+    let b: u32 = second
+        .trim()
+        .parse()
+        .expect("Error parsing: may not a valid number");
 
     let result = sum(a, b);
     println!("{} + {} = {}", a, b, result);
+
+    let res = divide(5, 0);
+    let value = res.expect("Failed to divide");
+    println!("Result: {}", value);
 }
 
 fn sum(a: u32, b: u32) -> u32 {
     a + b
+}
+
+fn divide(a: i32, b: i32) -> Result<i32, String> {
+    if b == 0 {
+        return Err(String::from("Cannot divide by zero"));
+    }
+    Ok(a / b)
 }
