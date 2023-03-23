b7faf9649f6b3ad24deed43fbdfa74e7e2696fc0
augustine <augustino0890@gmail.com>
2023-03-23 18:50:05 +0900

    space invaders: the player

--- crash/invaders/src/lib.rs
+++ crash/invaders/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod frame;
 pub mod render;
+pub mod player;
 
 pub const NUM_ROWS: usize = 20;
 pub const NUM_COLS: usize = 40;
--- crash/invaders/src/main.rs
+++ crash/invaders/src/main.rs
@@ -6,7 +6,11 @@ use crossterm::{
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
-use invaders::{frame::{self, new_frame}, render};
+use invaders::{
+    frame::{self, new_frame, Drawable}, 
+    render,
+    player::Player,
+};
 use rusty_audio::Audio;
 use std::{
     error::Error,
@@ -46,14 +50,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
     
     // Game loop
+    let mut player = Player::new();
     'gameloop: loop {
         // Per-frame init
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop
@@ -64,6 +71,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // Draw and render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
