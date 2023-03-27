4e6d1d4a4d79008440fa5751ebe1ff240628c8cb
augustine <augustino0890@gmail.com>
2023-03-27 17:33:27 +0900

    essential training: boolean data types

--- essential-training/src/main.rs
+++ essential-training/src/main.rs
@@ -23,4 +23,23 @@ fn main() {
 
     value = value >> 2;
     println!("The binary representation: {:08b}", value);
+
+    let a = true | false;
+    println!("true bitwise OR false is {}", a);
+    let b = true || false;
+    println!("true logical OR false is {}", b);
+
+    // The | operator operates on integers at the bit level, while || operates on boolean values.
+    let x = 0b1010; // This is binary literal for decimal 10
+    let y = 0b1100; // This is binary literal for decimal 12
+    // Bitwise OR
+    let z = x | y;
+    println!("Bitwise OR: {}", z); // Output: 14 (binary 1110)
+
+    // Logical OR
+    if x > 5 || y > 15 {
+        println!("At least one operand is true");
+    } else {
+        println!("Both operands are false");
+    }
 }
