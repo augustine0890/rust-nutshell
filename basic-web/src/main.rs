741bbe0409e49b25d35dcf882a9bdbce41786664
augustine <augustino0890@gmail.com>
2023-04-06 19:06:27 +0900

    run multiple actix app on different ports

--- basic-web/src/main.rs
+++ basic-web/src/main.rs
@@ -1,21 +1,32 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use futures::future;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", name)
 }
 
-#[actix_web::main]
+async fn health() -> impl Responder {
+    "All good\n"
+}
+
+#[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        println!("http server factory is firing");
+    let s1 = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
             .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
     })
     .bind("127.0.0.1:8080")?
-    .workers(3)
-    .run()
-    .await
+    .run();
+
+    let s2 = HttpServer::new(move || {
+        App::new().service(web::resource("/health").route(web::get().to(health)))
+    })
+    .bind("127.0.0.1:8000")?
+    .run();
+
+    future::try_join(s1, s2).await?;
+    Ok(())
 }
