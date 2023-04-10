ac04a77538c44505da98f06686f933c5c70a8c31
augustine <augustino0890@gmail.com>
2023-04-10 11:50:08 +0900

    axum (web application framework): Hello, world

---
+++ axum-project/hello-world/src/main.rs
@@ -0,0 +1,21 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+    // let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    // Address that server will bind to
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, world!"
+}
