7576c7c160c489cefc7e231c782afdfbb3bd7b67
augustine <augustino0890@gmail.com>
2023-04-26 18:41:30 +0900

    rustling: enums3

--- rustling/src/enums.rs
+++ rustling/src/enums.rs
@@ -1,15 +1,54 @@
 #![allow(dead_code)]
 #[derive(Debug)]
 enum Message {
-    Move { x: i32, y: i32 },
+    Move(Point),
     Echo(String),
-    ChangeColor(u8, u8, u8),
+    ChangeColor((u8, u8, u8)),
     Quit,
 }
 
 impl Message {
     fn call(&self) {
         println!("{:?}", &self);
+    }
+}
+
+#[derive(Debug)]
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+struct State {
+    color: (u8, u8, u8),
+    position: Point,
+    quit: bool,
+}
+
+impl State {
+    fn change_color(&mut self, color: (u8, u8, u8)) {
+        self.color = color;
+    }
+
+    fn quit(&mut self) {
+        self.quit = true;
+    }
+
+    fn echo(&self, s: String) {
+        println!("{}", s);
+    }
+
+    fn move_position(&mut self, p: Point) {
+        self.position = p;
+    }
+
+    fn process(&mut self, message: Message) {
+        match message {
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
@@ -20,13 +59,31 @@ mod tests {
     #[test]
     fn message_test() {
         let messages = [
-            Message::Move { x: 10, y: 30 },
+            Message::Move(Point { x: 10, y: 30 }),
             Message::Echo(String::from("hello world")),
-            Message::ChangeColor(200, 255, 255),
+            Message::ChangeColor((200, 255, 255)),
             Message::Quit,
         ];
         for message in messages {
             message.call();
         }
+    }
+
+    #[test]
+    fn test_match_message_call() {
+        let mut state = State {
+            quit: false,
+            position: Point { x: 0, y: 0 },
+            color: (0, 0, 0),
+        };
+        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::Echo(String::from("hello world")));
+        state.process(Message::Move(Point { x: 10, y: 15 }));
+        state.process(Message::Quit);
+
+        assert_eq!(state.color, (255, 0, 255));
+        assert_eq!(state.position.x, 10);
+        assert_eq!(state.position.y, 15);
+        assert_eq!(state.quit, true);
     }
 }
