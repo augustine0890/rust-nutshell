776cc653286801825140a0904961c27a1e8639c1
augustine <augustino0890@gmail.com>
2023-04-27 15:54:01 +0900

    rustling: strings2

--- rustling/src/strings.rs
+++ rustling/src/strings.rs
@@ -4,12 +4,23 @@ fn current_favorite_color() -> String {
     "blue".to_string()
 }
 
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "blue" || attempt == "green" || attempt == "red"
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn run_test() {
+        let word = String::from("green");
+        if is_a_color_word(&word) {
+            println!("That is a color word I know!");
+        } else {
+            println!("That is not a color word I know!");
+        }
+
         let answer = current_favorite_color();
         assert_eq!(answer, String::from("blue"));
     }
