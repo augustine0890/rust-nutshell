df5190394d7b53c01a15508452c4fc2c7a1fd5c2
augustine <augustino0890@gmail.com>
2023-04-28 14:42:49 +0900

    rustling: hashmaps2

--- rustling/src/hashmaps.rs
+++ rustling/src/hashmaps.rs
@@ -10,7 +10,7 @@ enum Fruit {
     Pineapple,
 }
 
-fn fruit_basket() -> HashMap<String, u32> {
+fn fruit_basket_v1() -> HashMap<String, u32> {
     let mut basket = HashMap::new();
     basket.insert(String::from("banana"), 2);
     basket.insert(String::from("apple"), 3);
@@ -20,19 +20,68 @@ fn fruit_basket() -> HashMap<String, u32> {
     basket
 }
 
+fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
+    let fruit_kinds = vec![
+        Fruit::Apple,
+        Fruit::Banana,
+        Fruit::Mango,
+        Fruit::Lychee,
+        Fruit::Pineapple,
+    ];
+
+    for fruit in fruit_kinds {
+        basket.entry(fruit).or_insert(2);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // Don't modify this function!
+    fn get_fruit_basket() -> HashMap<Fruit, u32> {
+        let mut basket = HashMap::<Fruit, u32>::new();
+        basket.insert(Fruit::Apple, 4);
+        basket.insert(Fruit::Mango, 2);
+        basket.insert(Fruit::Lychee, 5);
+
+        basket
+    }
+
+    #[test]
+    fn test_given_fruits_are_not_modified() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
+        assert_eq!(*basket.get(&Fruit::Mango).unwrap(), 2);
+        assert_eq!(*basket.get(&Fruit::Lychee).unwrap(), 5);
+    }
+
+    #[test]
+    fn at_least_five_types_of_fruits() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        let count_fruit_kinds = basket.len();
+        assert!(count_fruit_kinds >= 5);
+    }
+
+    #[test]
+    fn greater_than_eleven_fruits() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        let count = basket.values().sum::<u32>();
+        assert!(count > 11);
+    }
+
     #[test]
     fn at_least_three_types_of_fruits() {
-        let basket = fruit_basket();
+        let basket = fruit_basket_v1();
         assert!(basket.len() >= 3);
     }
 
     #[test]
     fn at_least_five_fruits() {
-        let basket = fruit_basket();
+        let basket = fruit_basket_v1();
         assert!(basket.values().sum::<u32>() >= 5);
     }
 }
