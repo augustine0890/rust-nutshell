f2c8532e8dca958ab5e9dc92f2a3a4cb7c81ec79
augustine <augustino0890@gmail.com>
2023-05-02 16:49:58 +0900

    rustling: errors2

--- rustling/src/errors.rs
+++ rustling/src/errors.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+use std::num::ParseIntError;
 
 pub fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
@@ -8,9 +9,37 @@ pub fn generate_nametag_text(name: String) -> Result<String, String> {
     }
 }
 
+pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let processing_fee = 1;
+    let cost_per_item = 5;
+    // let qty = match item_quantity.parse::<i32>() {
+    // Ok(q) => q,
+    // Err(err) => return Err(err),
+    // };
+    // let qty = item_quantity.parse::<i32>()?;
+
+    // Ok(qty * cost_per_item + processing_fee)
+    item_quantity
+        .parse::<i32>()
+        .map(|qty| qty * cost_per_item + processing_fee)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn item_quantity_is_a_valid_number() {
+        assert_eq!(total_cost("34"), Ok(171));
+    }
+
+    #[test]
+    fn item_quantity_is_an_invalid_number() {
+        assert_eq!(
+            total_cost("beep boop").unwrap_err().to_string(),
+            "invalid digit found in string"
+        );
+    }
 
     #[test]
     fn generates_nametag_text_for_a_nonempty_name() {
