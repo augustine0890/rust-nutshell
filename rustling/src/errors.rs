679acf4ca691356c32ee2faa3d2cd7efdb853fdf
augustine <augustino0890@gmail.com>
2023-05-02 17:21:31 +0900

    rustling: errors4

--- rustling/src/errors.rs
+++ rustling/src/errors.rs
@@ -41,9 +41,39 @@ fn spend_total_cost() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        match value {
+            num if num < 0 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            value => Ok(PositiveNonzeroInteger(value as u64)),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_creation() {
+        assert!(PositiveNonzeroInteger::new(10).is_ok());
+        assert_eq!(
+            Err(CreationError::Negative),
+            PositiveNonzeroInteger::new(-10)
+        );
+        assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+    }
+
     #[test]
     fn test_spend_total_cost() {
         let result = spend_total_cost();
