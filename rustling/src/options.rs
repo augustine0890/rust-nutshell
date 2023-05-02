cea938bc0bcad92c0767e4cb0ce335d346c54c3f
augustine <augustino0890@gmail.com>
2023-05-02 15:39:18 +0900

    rustling: option2

--- rustling/src/options.rs
+++ rustling/src/options.rs
@@ -18,6 +18,33 @@ mod tests {
     use super::*;
 
     #[test]
+    fn simple_option() {
+        let target = "rustlings";
+        let optional_target = Some(target);
+
+        // TODO: Make this an if let statement whose value is "Some" type
+        if let Some(word) = optional_target {
+            assert_eq!(word, target);
+        }
+    }
+
+    #[test]
+    fn layered_option() {
+        let mut range = 10;
+        let mut optional_integers: Vec<Option<i8>> = Vec::new();
+        for i in 0..(range + 1) {
+            optional_integers.push(Some(i));
+        }
+
+        // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
+        // You can stack `Option<T>`'s into while let and if let
+        while let Some(Some(integer)) = optional_integers.pop() {
+            assert_eq!(integer, range);
+            range -= 1;
+        }
+    }
+
+    #[test]
     fn check_icecream() {
         assert_eq!(maybe_icecream(9), Some(5));
         assert_eq!(maybe_icecream(10), Some(5));
