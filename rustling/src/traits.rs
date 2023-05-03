ea80bae48f691d8a0f363c20650adb32be88fd65
augustine <augustino0890@gmail.com>
2023-05-03 18:23:28 +0900

    rustling: traits5

--- rustling/src/traits.rs
+++ rustling/src/traits.rs
@@ -38,9 +38,39 @@ fn compare_license_types(software: impl Licensed, software_two: impl Licensed) -
     software.licensing_info() == software_two.licensing_info()
 }
 
+pub trait SomeTrait {
+    fn some_function(&self) -> bool {
+        true
+    }
+}
+
+pub trait OtherTrait {
+    fn other_function(&self) -> bool {
+        true
+    }
+}
+
+struct SomeStruct {}
+struct OtherStruct {}
+
+impl SomeTrait for SomeStruct {}
+impl OtherTrait for SomeStruct {}
+impl SomeTrait for OtherStruct {}
+impl OtherTrait for OtherStruct {}
+
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
+    item.some_function() && item.other_function()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_traits() {
+        some_func(SomeStruct {});
+        some_func(OtherStruct {});
+    }
 
     #[test]
     fn compare_license_information() {
