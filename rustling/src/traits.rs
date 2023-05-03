fff8d026b40bae234fd1124828f573d2caeeae15
augustine <augustino0890@gmail.com>
2023-05-03 18:13:20 +0900

    rustling: traits4

--- rustling/src/traits.rs
+++ rustling/src/traits.rs
@@ -17,9 +17,61 @@ impl AppendBar for Vec<String> {
     }
 }
 
+pub trait Licensed {
+    fn licensing_info(&self) -> String {
+        "Some information".to_string()
+    }
+}
+
+struct SomeSoftware {
+    version_number: i32,
+}
+
+struct OtherSoftware {
+    version_number: String,
+}
+
+impl Licensed for SomeSoftware {} // Don't edit this line
+impl Licensed for OtherSoftware {} // Don't edit this line
+
+fn compare_license_types(software: impl Licensed, software_two: impl Licensed) -> bool {
+    software.licensing_info() == software_two.licensing_info()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn compare_license_information() {
+        let some_software = SomeSoftware { version_number: 1 };
+        let other_software = OtherSoftware {
+            version_number: "1.0.0".to_string(),
+        };
+
+        assert!(compare_license_types(some_software, other_software));
+    }
+
+    #[test]
+    fn compare_license_information_backwards() {
+        let some_software = SomeSoftware { version_number: 1 };
+        let other_software = OtherSoftware {
+            version_number: "1.0.0".to_string(),
+        };
+
+        assert!(compare_license_types(other_software, some_software));
+    }
+
+    #[test]
+    fn is_licensing_info_the_same() {
+        let licensing_info = String::from("Some information");
+        let some_software = SomeSoftware { version_number: 1 };
+        let other_software = OtherSoftware {
+            version_number: "v2.0.0".to_string(),
+        };
+        assert_eq!(some_software.licensing_info(), licensing_info);
+        assert_eq!(other_software.licensing_info(), licensing_info);
+    }
 
     #[test]
     fn is_vec_pop_eq_bar() {
