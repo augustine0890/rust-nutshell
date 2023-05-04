bb5366bbe5caa84c84f9508199c57938f923058d
augustine <augustino0890@gmail.com>
2023-05-04 17:44:04 +0900

    ownership: copy and clone traits

--- crash/ownership/src/ownership.rs
+++ crash/ownership/src/ownership.rs
@@ -19,16 +19,49 @@ fn print_string(a: &String) {
     println!("This is the value of my string: {}", a);
 }
 
+#[derive(Debug)]
+struct Person {
+    id: u8,
+    age: u8,
+    name: String,
+}
+
+impl Clone for Person {
+    fn clone(&self) -> Self {
+        println!("Copied from this old value {:?}", self);
+
+        Person {
+            id: self.id,
+            age: self.age,
+            name: self.name.clone(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test() {
-        rule_ownership();
+        let p1 = Person {
+            id: 1,
+            age: 29,
+            name: "augustine".to_string(),
+        };
+
+        let mut p2 = p1.clone();
+        p2.id = 2;
+        p2.age = 32;
+        p2.name = "jack".to_string();
+
+        println!("{:?}", p1);
+        println!("{:?}", p2);
 
         let x = String::from("41");
         print_string(&x);
         print_string(&x);
+
+        rule_ownership();
     }
 }
