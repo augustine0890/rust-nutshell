dceaee2ea2f98f07ff0403397b044732bccc4546
augustine <augustino0890@gmail.com>
2023-05-08 18:29:34 +0900

    rustling: lifetimes3

--- rustling/src/lifetimes.rs
+++ rustling/src/lifetimes.rs
@@ -8,9 +8,26 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_book_struct() {
+        let name = String::from("Jill Smith");
+        let title = String::from("Fish Flying");
+        let book = Book {
+            author: &name,
+            title: &title,
+        };
+
+        println!("{} by {}", book.title, book.author);
+    }
 
     #[test]
     fn test_longest() {
