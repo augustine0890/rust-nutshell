7b7f5b58dfe2567a1804656283946399f25e97b2
augustine <augustino0890@gmail.com>
2023-05-08 18:25:07 +0900

    rustling: lifetimes2

--- rustling/src/lifetimes.rs
+++ rustling/src/lifetimes.rs
@@ -16,7 +16,19 @@ mod tests {
     fn test_longest() {
         let string1 = String::from("abcd");
         let string2 = "xyz";
+
         let result = longest(string1.as_str(), string2);
         println!("The longest string is: {}", result);
+    }
+
+    #[test]
+    fn test_longest_2() {
+        let string1 = String::from("long string is long");
+        let result;
+        {
+            let string2 = String::from("xyz");
+            result = longest(string1.as_str(), string2.as_str());
+            println!("The longest string is '{}'", result);
+        }
     }
 }
