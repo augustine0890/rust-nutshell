df612a8db21d309234ae236afb1226c620565446
augustine <augustino0890@gmail.com>
2023-05-08 17:56:36 +0900

    ownership: lifetime elision rules

--- crash/ownership/src/lifetime.rs
+++ crash/ownership/src/lifetime.rs
@@ -21,12 +21,44 @@ fn get_oldest<'a>(person1: &'a Person<'a>, person2: &'a Person<'a>) -> &'a Perso
     }
 }
 
+impl<'a> Person<'a> {
+    fn print_and_return(&self, announcement: &str) -> &str {
+        println!("{}", announcement);
+        println!("{}'s favorite fruit is {}", self.name, self.favorite_fruit);
+        self.favorite_fruit
+    }
+}
+/*
+Lifetime Elision Rules
+1. The compiler assigns a lifetime parameter to each parameter that's a reference.
+2. If there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters.
+3. If there are multiple input lifetime parameters, but one of them is &self of &mut self, the lifetime of self is assigned
+to all output lifetime parameters. */
+fn first_word_in_string(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    s
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn run_test() {
+        let sentences = vec!["Hello world!", "How are you?", "I am fine", "Good"];
+        for sentence in sentences.iter() {
+            println!(
+                "The first word in the [{}] is: {}",
+                sentence,
+                first_word_in_string(sentence)
+            );
+        }
+
         let fruits = vec!["Apples", "Clementines", "Grapefruit", "Kiwis"];
         let p1 = Person {
             name: "John".to_string(),
@@ -45,5 +77,6 @@ mod tests {
         }
         println!("p1: {:?}", p1);
         println!("p2: {:?}", p2);
+        p2.print_and_return("Everyone listen up!");
     }
 }
