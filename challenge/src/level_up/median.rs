921c24eea101b053c8cfbc44ae6ab9c1db0d0073
augustine <augustino0890@gmail.com>
2023-05-09 18:08:58 +0900

    challenges: calculate the median

--- challenge/src/level_up/median.rs
+++ challenge/src/level_up/median.rs
@@ -1,23 +1,54 @@
-pub fn median(a: Vec<f32>) -> Option<f32> {
-    let len = a.len();
-    if len == 0 {
-        None
-    } else {
-        let mut sorted_a = a.clone();
-        sorted_a.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        let mid = len / 2;
-        if mid % 2 == 0 {
-            Some((sorted_a[mid - 1] + sorted_a[mid]) / 2.0)
-        } else {
-            Some(sorted_a[mid])
-        }
+#![allow(dead_code)]
+pub fn median(mut a: Vec<f32>) -> Option<f32> {
+    if a.is_empty() {
+        return None;
     }
+
+    a.sort_by(|x, y| x.partial_cmp(y).unwrap());
+    let n_elements = a.len();
+    let middle = n_elements / 2;
+    let med = if n_elements % 2 == 0 {
+        (a[middle - 1] + a[middle]) / 2.0
+    } else {
+        a[middle]
+    };
+
+    Some(med)
 }
 
-#[test]
-fn empty_list() {
-    let input = vec![];
-    let expected_output = None;
-    let actutal_output = median(input);
-    assert_eq!(actutal_output, expected_output);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_list() {
+        let input = vec![];
+        let expected_output = None;
+        let actutal_output = median(input);
+        assert_eq!(actutal_output, expected_output);
+    }
+
+    #[test]
+    fn sorted_list() {
+        let input = vec![1.0, 4.0, 5.0];
+        let expected_output = Some(4.0);
+        let actutal_output = median(input);
+        assert_eq!(actutal_output, expected_output);
+    }
+
+    #[test]
+    fn even_length() {
+        let input = vec![1.0, 3.0, 5.0, 6.0];
+        let expected_output = Some(4.0);
+        let actual_output = median(input);
+        assert_eq!(actual_output, expected_output);
+    }
+
+    #[test]
+    fn unsorted_list() {
+        let input = vec![1.0, 5.0, 2.0];
+        let expected_output = Some(2.0);
+        let actual_output = median(input);
+        assert_eq!(actual_output, expected_output);
+    }
 }
--- challenge/src/main.rs
+++ challenge/src/main.rs
@@ -1,9 +1,3 @@
 mod level_up;
 
-use level_up::*; // Import all the re-exported functions from level_up
-
-fn main() {
-    let answer = median(vec![1.0, 2.0, 5.0]);
-
-    println!("median([1,2,5]) = {:?}", answer);
-}
+fn main() {}
